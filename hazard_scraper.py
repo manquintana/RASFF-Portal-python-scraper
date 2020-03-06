aba0b95b6d81fd25c620c857588cc92095fdf76d
jevi92034 <manuelalejandroquintana@gmail.com>
2020-03-06 09:55:01 -0300

    FInished project

--- hazard_scraper.py
+++ hazard_scraper.py
@@ -12,11 +12,19 @@ from selenium.webdriver.common.keys import Keys
 import math
 import pandas as pd
 import time
+import sqlalchemy
+
+# Base local
+DB_USER = 'manuel'
+DB_PASS = 'jevi1717'
+DB_ADDR = 'localhost'
+DB_NAME = 'rasff_hazards'
+DB_CONN = sqlalchemy.create_engine('mysql+mysqlconnector://{0}:{1}@{2}/{3}'.format(DB_USER, DB_PASS, DB_ADDR, DB_NAME))
+
 
 driver = webdriver.Chrome(executable_path='/home/manuel/3. git/RASFF_portal_python_scrapper/chromedriver')
 driver.get('https://webgate.ec.europa.eu/rasff-window/portal/#')
 
-datos = pd.DataFrame(columns=['Reference','Subject','HazardCategory'])
   
 get_results = driver.find_element_by_id("Btn_Search")
 get_results.click()
@@ -27,6 +35,18 @@ numbs = [int(s) for s in wanted_title.split() if s.isdigit()] #Extracting number
 results_number = numbs[0]
 print('Cantidad de resultados: {0}'.format(results_number))
 
+def append_to_table(data, table_name):
+    print('| Updating table: ' + table_name + '...')
+    total_rows= len(data)
+    for i in range(total_rows):
+        try:
+            print ("| Appending row #" + str(i+1) + " of " + str(total_rows))
+            data.iloc[i:i+1].to_sql(con=DB_CONN, name='subject_hazard', if_exists='append', index = False)
+        except:
+            print("| >> Data Error" + str(i+1))
+            print('| Content with error: ' + data.iloc[i:i+1])
+    print(' ')
+    print(' ')
 
 # #########################################################
 # PART 1: GETTING REFERENCE CODES FROM ALL PAGES OF RESULTS
@@ -37,16 +57,31 @@ hazards = []
 current_page = 0
 total_pages = math.ceil(results_number/100)
 
+#Restore after error!!!
+error_page= 100 #Set error page. 0 if not error!!!!!!!!!!!!   #ME SALTEE LA 5,9,14 QUE FALLABA UN DATO SIEMPRE!!! ERA ilegal pork....
+for i in range(1,error_page): #hay que seguir scrapeando!
+    time.sleep(1)
+    results_number = results_number - 100
+    current_page = current_page + 1 
+    next_page = driver.find_element_by_link_text("Next 100")
+    next_page.click()
+
 while(results_number > 0):
     current_page = current_page + 1 
     #print('Agregando codigos de la pagina {0} de {1}'.format(current_page, total_pages))
     rows = driver.find_elements_by_xpath('//tr') #selecting rows from table
+    
+       
     print('>>>')
     print('Agregando codigos de la pagina {0} de {1}'.format(current_page, total_pages))
+    datos = pd.DataFrame(columns=['Reference','Subject','HazardCategory'])
+
+
+
     for i in range(1,len(rows)):
         columns = rows[i].find_elements_by_css_selector("td")
         current_reference_code = columns[3].text
-        current_subject = columns[5].text\
+        current_subject = columns[5].text
         
         #print(current_reference_code)
         #print(current_subject)
@@ -75,14 +110,20 @@ while(results_number > 0):
         driver.close() #Close new tab
         driver.switch_to.window(driver.window_handles[0]) #return to original tab
 
-    results_number = results_number - 100
+    #Persist data
     print('Data scrapped:')
-    print(datos)   
+    print(datos)
+    print('Saving data to Database')
+    datos.rename(columns = {'Reference':'reference', 'Subject':'subject', 'HazardCategory':'hazard_category'}, inplace = True) 
+    append_to_table(datos, 'subject_hazard')
+    
+
+
+    results_number = results_number - 100
     if results_number > 0: #hay que seguir scrapeando!
         next_page = driver.find_element_by_link_text("Next 100")
         next_page.click()
     #Ya levante toda la data de la pagina actual
 
+print('The End!')
 
-print('All the data has been scrapped! Saving to file datascrapped.csv')
-datos.to_csv('datascrapped.csv')
